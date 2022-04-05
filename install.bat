f4d7fe2f4b04a540dd704eab60e96b7299d6d984
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-05 20:55:28 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
-title Version 1.3.6 - Source Code: github.com/thekevie/school-programs
+title Version 1.3.7 - Source Code: github.com/thekevie/school-programs
 set directory=%CD%
 set startup=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 
