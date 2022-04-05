fa7d725728050e0e05d32adcd9a5672ce01527dd
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-05 17:03:49 +0200

    Update and rename program.bat to desktop.bat

---
+++ desktop.bat
@@ -0,0 +1,19 @@
+// Desktop Version v1.1.0
+@echo off
+title Desktop
+cd /d %userprofile%\Desktop
+del /q /f www.studi.se.url
+del /q /f Landguiden.url
+cd /d %public%/Desktop
+del /q /f www.studi.se.url
+del /q /f Landguiden.url
+
+cls
+cd %temp%
+curl -OL https://raw.githubusercontent.com/thekevie/school-program/main/desktop.bat
+cd %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+if not exist desktop.bat (
+    type nul > desktop.bat
+)
+copy /Y "%temp%\desktop.bat" "desktop.bat"
+del /q /f %temp%\desktop.bat
