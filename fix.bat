fd401b57eff85fadbb4778a5e16ea4037b4484ca
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-05 14:09:21 +0200

    Update fix.bat

--- fix.bat
+++ fix.bat
@@ -6,4 +6,8 @@ del /q /f Landguiden.url
 cd /d %public%/Desktop
 del /q /f www.studi.se.url
 del /q /f Landguiden.url
+
+curl https://raw.githubusercontent.com/thekevie/school-fix/main/fix.bat -O check.txt
+fc fix.txt fix.bat
+pause
 exit
