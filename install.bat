fa2c540e1056b7b710738ef98a3b507e6aed4daa
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-05 17:16:33 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,22 +5,22 @@ set directory=%CD%
 :menu
 echo 1. Install
 echo 2. Uninstall
-echo 3. Update
-echo 4. Support
+echo 3. Support
+echo 4. Hide
 echo 5. Exit
 set /p choices=Type the number: 
 if '%choices%' == '1' goto install
 if '%choices%' == '2' goto uninstall
-if '%choices%' == '3' goto update
-if '%choices%' == '4' goto support
+if '%choices%' == '3' goto support
+if '%choices%' == '3' goto hide
 if '%choices%' == '5' goto exit
 cls
 goto menu
 
 :install
 cd %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
-del /q /f fix.bat
-curl -OL https://raw.githubusercontent.com/thekevie/school-fix/main/fix.bat
+del /q /f program.bat
+curl -OL https://raw.githubusercontent.com/thekevie/school-programs/main/desktop.bat
 cls
 echo Installation Done
 echo.
@@ -28,23 +28,11 @@ goto menu
 
 :uninstall
 cd %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
-del /q /f fix.bat
+del /q /f desktop.bat
 cls
 echo Uninstallation Done
 echo.
 goto menu
-
-:update
-cls
-echo When the update is done the file will close
-timeout /t 5 > nul
-cls
-cd %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
-del /q /f fix.bat
-curl -OL https://raw.githubusercontent.com/thekevie/school-fix/main/fix.bat
-cd %directory%
-curl -OL https://raw.githubusercontent.com/thekevie/school-fix/main/install-fix.bat
-exit
 
 :support
 cls
@@ -52,6 +40,9 @@ echo Discord: kevie#9091
 echo Github: thekevie
 echo.
 goto menu
+
+:hide
+echo Coming Soon
 
 :exit
 exit
