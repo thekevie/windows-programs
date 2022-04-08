1d72323cddb6d5c6468f1720ec95485964fa8e7e
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-08 13:08:48 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
-title Version 1.6.5 - Source Code: github.com/thekevie/school-programs
+title Version 1.6.6 - Source Code: github.com/thekevie/school-programs
 set directory=%CD%
 set startup=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 set plugindir=%userprofile%\AppData
@@ -205,7 +205,8 @@ cls
 goto Admin
 
 :AdminSchtasks
-schtasks /query /TN desktop
+set /p taskname=Task Name: 
+schtasks /query /tn %taskname%
 echo.
 echo.
 goto Admin
