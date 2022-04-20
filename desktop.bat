ead7304311d0d7c434098387ef1c4ce56f6407d8
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-20 12:33:22 +0200

    Update desktop.bat

--- desktop.bat
+++ desktop.bat
@@ -1,6 +1,8 @@
 @echo off
 
-title Desktop Plugin v1.2.8
+title Desktop Plugin v1.2.9
+
+:loop
 
 del /q %public%\desktop\www.studi.se.url
 del /q %userprofile%\desktop\www.studi.se.url
@@ -9,3 +11,4 @@ del /q %public%\desktop\Landguiden.url
 del /q %userprofile%\desktop\Landguiden.url
 
 curl -OL https://raw.githubusercontent.com/thekevie/school-programs/main/desktop.bat
+goto loop
