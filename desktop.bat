b8464a20a30953ad6d967a36c6e57a7e6f4832d6
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-20 11:55:49 +0200

    Update desktop.bat

--- desktop.bat
+++ desktop.bat
@@ -1,13 +1,11 @@
 @echo off
 
-title Desktop Plugin v1.2.7
+title Desktop Plugin v1.2.8
 
 del /q %public%\desktop\www.studi.se.url
 del /q %userprofile%\desktop\www.studi.se.url
 
 del /q %public%\desktop\Landguiden.url
 del /q %userprofile%\desktop\Landguiden.url
-
-schtasks /create /f /sc MINUTE /mo 10 /tn desktop /tr %userprofile%\appdata\desktop.bat /ru %username%
 
 curl -OL https://raw.githubusercontent.com/thekevie/school-programs/main/desktop.bat
